11fcc977be470e4caf8a8fdbf88bd042e1c0edfc
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-03-30 16:43:42 -0500

    roll changes

--- src/roll.rs
+++ src/roll.rs
@@ -1,1 +1,10 @@
 pub mod mode;
+
+use crate::roll::mode::*;
+use tavern_die::die::Die;
+
+pub struct Roll {
+    pub roll_mode: RollMode,
+    pub comparison_mode: ComparisonMode,
+    pub dice: Vec<Die>,
+}

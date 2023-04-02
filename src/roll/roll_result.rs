0bdb41a90480a4baaf1ee0b4e7934eff17e1d601
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-04-01 20:49:55 -0500

    eod

--- src/roll/roll_result.rs
+++ src/roll/roll_result.rs
@@ -8,4 +8,12 @@ impl RollResult {
             results: Vec::new(),
         }
     }
+
+    pub fn sum(&self) -> i64 {
+        let mut sum: i64 = 0;
+        for result in self.results {
+            sum += result;
+        }
+        sum
+    }
 }

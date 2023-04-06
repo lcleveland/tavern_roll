615ce65c2b8d0d3c5d79911bfe416100a50a93f5
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-04-06 16:31:58 -0500

    add tests

--- tests/roll_integration_test.rs
+++ tests/roll_integration_test.rs
@@ -7,7 +7,7 @@ use tavern_roll::roll::Roll;
 
 #[test]
 fn normal_equal_test() {
-    let die_rolls = vec![1];
+    let die_rolls = vec![69];
     let die = common::create_test_die(
         die_rolls,
         die::mode::RollMode::Normal,
@@ -17,5 +17,20 @@ fn normal_equal_test() {
     roll.roll_mode = RollMode::Normal;
     roll.comparison_mode = ComparisonMode::Equal(0);
     roll.dice.push(DieRoll::new(die, 1));
-    assert_eq!(roll.roll().sum(), 1);
+    assert_eq!(roll.roll().sum(), 69);
+}
+
+#[test]
+fn normal_greater_than_test() {
+    let die_rolls = vec![1, 31, 38];
+    let die = common::create_test_die(
+        die_rolls,
+        die::mode::RollMode::Normal,
+        die::mode::ComparisonMode::Equal(0),
+    );
+    let mut roll = Roll::default();
+    roll.roll_mode = RollMode::Normal;
+    roll.comparison_mode = ComparisonMode::GreaterThan(30);
+    roll.dice.push(DieRoll::new(die, 3));
+    assert_eq!(roll.roll().sum(), 69);
 }

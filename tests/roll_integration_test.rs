f5aee41b1ff01e352b1cee8f2fa80129f5d9a1f2
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-04-06 16:58:56 -0500

    fix tests

--- tests/roll_integration_test.rs
+++ tests/roll_integration_test.rs
@@ -22,7 +22,7 @@ fn normal_equal_test() {
 
 #[test]
 fn normal_greater_than_test() {
-    let die_rolls = vec![1, 31, 38];
+    let die_rolls = vec![1, 31, 37];
     let die = common::create_test_die(
         die_rolls,
         die::mode::RollMode::Normal,
@@ -32,5 +32,35 @@ fn normal_greater_than_test() {
     roll.roll_mode = RollMode::Normal;
     roll.comparison_mode = ComparisonMode::GreaterThan(30);
     roll.dice.push(DieRoll::new(die, 3));
+    assert_eq!(roll.roll().sum(), 69);
+}
+
+#[test]
+fn normal_less_than_test() {
+    let die_rolls = vec![20, 30, 19];
+    let die = common::create_test_die(
+        die_rolls,
+        die::mode::RollMode::Normal,
+        die::mode::ComparisonMode::Equal(0),
+    );
+    let mut roll = Roll::default();
+    roll.roll_mode = RollMode::Normal;
+    roll.comparison_mode = ComparisonMode::LessThan(70);
+    roll.dice.push(DieRoll::new(die, 3));
+    assert_eq!(roll.roll().sum(), 69);
+}
+
+#[test]
+fn reroll_equal_test() {
+    let die_rolls = vec![15, 15, 39, 30];
+    let die = common::create_test_die(
+        die_rolls,
+        die::mode::RollMode::Normal,
+        die::mode::ComparisonMode::Equal(0),
+    );
+    let mut roll = Roll::default();
+    roll.roll_mode = RollMode::Reroll;
+    roll.comparison_mode = ComparisonMode::Equal(30);
+    roll.dice.push(DieRoll::new(die, 2));
     assert_eq!(roll.roll().sum(), 69);
 }

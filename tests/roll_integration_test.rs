6ff33469243a5cec1622e50744e223b1992425ac
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-04-06 12:56:31 -0500

    change for clarity

--- tests/roll_integration_test.rs
+++ tests/roll_integration_test.rs
@@ -1,6 +1,6 @@
 mod common;
 
-use tavern_die::die::mode;
+use tavern_die::die;
 use tavern_roll::roll::die_roll::DieRoll;
 use tavern_roll::roll::mode::*;
 use tavern_roll::roll::Roll;
@@ -10,8 +10,8 @@ fn normal_equal_test() {
     let die_rolls = vec![1];
     let die = common::create_test_die(
         die_rolls,
-        mode::RollMode::Normal,
-        mode::ComparisonMode::Equal(0),
+        die::mode::RollMode::Normal,
+        die::mode::ComparisonMode::Equal(0),
     );
     let mut roll = Roll::default();
     roll.roll_mode = RollMode::Normal;
